ddfb5d36b5f13ae10e034ba4def03270ea074c8f
Masood <aweaz7@gmail.com>
2022-06-29 11:42:59 +0530

    Create Remove Duplicates from Sorted Array.py

---
+++ Top Interview Questions/Remove Duplicates from Sorted Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        k = 0
+        while(i<len(nums)-1):
+            if nums[i]==nums[i+1]:
+                del nums[i+1]
+                k=k+1
+            else:
+                i=i+1
+        return len(nums)
+
