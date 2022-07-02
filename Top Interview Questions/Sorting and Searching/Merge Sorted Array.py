ac360d095fa5694495036be4b0fa13de04c71981
Masood <aweaz7@gmail.com>
2022-07-02 16:05:17 +0530

    Create Merge Sorted Array.py

---
+++ Top Interview Questions/Sorting and Searching/Merge Sorted Array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        temp = nums1[:m]
+        #print(temp)
+        for i in nums2:
+            temp.append(i)
+        #print(temp)
+        temp.sort()
+        #print(temp)
+        for i in range(len(temp)):
+            nums1[i]=temp[i]
+        
+#**********************************OR****************************************#       
+        
+        
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
+    
+            
+                
+
