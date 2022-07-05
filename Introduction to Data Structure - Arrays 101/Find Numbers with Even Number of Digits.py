62862ef2f5c895889ac6bdce414f409c02a698e7
Masood <aweaz7@gmail.com>
2022-07-05 15:47:28 +0530

    Create Find Numbers with Even Number of Digits.py

---
+++ Introduction to Data Structure - Arrays 101/Find Numbers with Even Number of Digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    
+    def findNumbers(self, nums: List[int]) -> int:
+        count=0
+        for i in nums:
+            digits=0
+            while(i):
+                i=i//10
+                digits+=1
+            if digits%2==0:
+                count+=1
+        return count
+        
