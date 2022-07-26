5627fbdef3b43e1629a1c00afd2f50038e5b4835
Masood <aweaz7@gmail.com>
2022-07-26 12:32:15 +0530

    Create 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py

---
+++ Trees/Easy/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py
@@ -0,0 +1,40 @@
+'''********************************************************************************** 
+Given two binary trees original and cloned and given a reference to a node target in the original tree.
+
+The cloned tree is a copy of the original tree.
+
+Return a reference to the same node in the cloned tree.
+
+Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
+
+ 
+
+Example 1:
+
+
+Input: tree = [7,4,3,null,null,6,19], target = 3
+Output: 3
+Explanation: In all examples the original and cloned trees are shown. The target node is a green node from the original tree. The answer is the yellow node from the cloned tree.
+**********************************************************************************'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        
+        self.ans = None
+        
+        def inorder(o,c):
+            if o:
+                inorder(o.left,c.left)
+                if o is target:
+                    self.ans = c
+                    return 
+                inorder(o.right,c.right)
+        inorder(original,cloned)
+        return self.ans
