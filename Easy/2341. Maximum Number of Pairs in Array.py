700a4340acaa81a778403293934e415763d32053
Masood <aweaz7@gmail.com>
2022-08-05 00:15:59 +0530

    Create 2341. Maximum Number of Pairs in Array.py

---
+++ Easy/2341. Maximum Number of Pairs in Array.py
@@ -0,0 +1,32 @@
+'''********************************************************************************** 
+You are given a 0-indexed integer array nums. In one operation, you may do the following:
+
+Choose two integers in nums that are equal.
+Remove both integers from nums, forming a pair.
+The operation is done on nums as many times as possible.
+
+Return a 0-indexed integer array answer of size 2 where answer[0] is the number of pairs that are formed and answer[1] is the number of leftover integers in nums after doing the operation as many times as possible.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2,1,3,2,2]
+Output: [3,1]
+Explanation:
+Form a pair with nums[0] and nums[3] and remove them from nums. Now, nums = [3,2,3,2,2].
+Form a pair with nums[0] and nums[2] and remove them from nums. Now, nums = [2,2,2].
+Form a pair with nums[0] and nums[1] and remove them from nums. Now, nums = [2].
+No more pairs can be formed. A total of 3 pairs have been formed, and there is 1 number leftover in nums.
+**********************************************************************************'''
+
+class Solution:
+    def numberOfPairs(self, nums: List[int]) -> List[int]:
+        d = dict()
+        for i in nums:
+            if i not in d:
+                d[i]=nums.count(i)%2
+        print(d)
+        c = sum(list(d.values()))
+        r = (len(nums)-c)//2
+        return [r,c]
