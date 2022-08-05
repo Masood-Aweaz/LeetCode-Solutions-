b66038bc40e7be2d5e7ae3dc5915754ed150310c
Masood <aweaz7@gmail.com>
2022-08-05 12:37:38 +0530

    Create 1971. Find if Path Exists in Graph.py

---
+++ Graphs/Easy/1971. Find if Path Exists in Graph.py
@@ -0,0 +1,40 @@
+'''********************************************************************************** 
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+**********************************************************************************'''
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        adj = defaultdict(list)
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        print(adj)
+        
+        visited = set()
+        queue=[source]
+        visited.add(source)
+        while queue!=[]:
+            tmp = queue.pop()
+            if tmp == destination:
+                return True
+            else:
+                for neigh in adj[tmp]:
+                    if neigh not in visited:
+                        queue.append(neigh)
+                        visited.add(neigh)
+        return False
