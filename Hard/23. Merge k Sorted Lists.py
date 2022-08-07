f3a2071b9fc16be1761a6418eac974da0d1003ce
Masood <aweaz7@gmail.com>
2022-08-07 19:10:56 +0530

    Create 23. Merge k Sorted Lists.py

---
+++ Hard/23. Merge k Sorted Lists.py
@@ -0,0 +1,38 @@
+'''********************************************************************************** 
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+**********************************************************************************'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        self.nodes =[]
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for i in sorted(self.nodes):
+            point.next = ListNode(i)
+            point = point.next
+        return head.next
