0f6d8e7351e3136773583fc6a0395b6d03f8d068
Masood <aweaz7@gmail.com>
2022-08-08 20:05:30 +0530

    Create 804. Unique Morse Code Words.py

---
+++ Hash Table/Easy/804. Unique Morse Code Words.py
@@ -0,0 +1,44 @@
+'''********************************************************************************** 
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+ 
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+**********************************************************************************'''
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        if len(words)==1:
+            return 1
+        m = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        dp = dict()
+        for i in range(len(m)):
+            dp[chr(97+i)]=m[i]
+        print(dp)
+        ans=set()
+        for i in range(len(words)):
+            res = ""
+            for j in words[i]:
+                res += dp[j]
+            ans.add(res)
+        return len(ans)
