665eb10876b04d8efb72503b2787749049d91aab
Masood <aweaz7@gmail.com>
2022-08-24 16:45:30 +0530

    Create 2331. Evaluate Boolean Binary Tree.py

---
+++ Trees/Easy/2331. Evaluate Boolean Binary Tree.py
@@ -0,0 +1,50 @@
+
+'''********************************************************************************** 
+You are given the root of a full binary tree with the following properties:
+
+Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+The evaluation of a node is as follows:
+
+If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+Return the boolean result of evaluating the root node.
+
+A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+A leaf node is a node that has zero children.
+
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3,null,null,0,1]
+Output: true
+Explanation: The above diagram illustrates the evaluation process.
+The AND node evaluates to False AND True = False.
+The OR node evaluates to True OR False = True.
+The root node evaluates to True, so we return true.
+**********************************************************************************'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        #self.flag = False
+        def dfs(node):
+            if not node.left:
+                return node.val
+            if node.val == 2:
+                res = dfs(node.left) or dfs(node.right)
+            elif node.val == 3:
+                res = dfs(node.left) and dfs(node.right)
+            return res
+            #print(node.val)
+            
+            
+        return dfs(root)
